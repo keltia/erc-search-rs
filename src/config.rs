97b9c3673aa4ca4a7625d9c72ec51ace4cc9c07a
Ollivier Robert <roberto@keltia.net>
2021-10-14 00:33:59 +0200

    Add test, comments and code.

--- src/config.rs
+++ src/config.rs
@@ -1,49 +1,60 @@
+//! Main configuration management and loading
+//!
+use std::collections::HashMap;
 use std::fs;
 
+use anyhow::Result;
 use serde::Deserialize;
-use toml::value::Table;
 
-#[derive(Debug, Deserialize)]
-pub struct Source {
-    domain: String,
-    site: String,
-    port: Option<u16>,
-    base: String,
-    filter: Option<String>,
-    attrs: Option<Vec<String>>,
-}
+use crate::source::Source;
 
-#[derive(Debug, Deserialize)]
+/// Main struct holding configurations
+#[derive(Debug, Deserialize, PartialEq)]
 pub struct Config {
     pub verbose: Option<bool>,
-    pub sources: Option<Table>,
+    pub sources: HashMap<String, Source>,
 }
 
+/// `Default` is for `unwrap_or_default()`.
+impl Default for Config {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+/// Methods for `Config`
 impl Config {
     pub fn new() -> Config {
         Config {
-            verbose: Option::None,
-            sources: Option::None,
+            verbose: Some(false),
+            sources: HashMap::new(),
         }
     }
 
     // That way, we do not panic whatever happens and return an empty Config.
-    pub fn load(fname: &str) -> Config {
-        let nul = Config::new();
-        let content = fs::read_to_string(fname);
+    pub fn load(fname: &str) -> anyhow::Result<Config> {
+        let content = fs::read_to_string(fname)?;
 
         println!("{:?}", content);
 
-        let content = match content {
-            Ok(content) => content,
-            Err(_) => return nul,
-        };
-        let cfg = toml::from_str(&content);
-        match cfg {
-            Ok(cfg) => cfg,
-            Err(_) => return nul,
-        }
+        let s: Config = toml::from_str(&content).unwrap_or_default();
+        Ok(s)
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
 
+    #[test]
+    fn test_new() {
+        let a = Config::new();
+        assert_eq!(
+            Config {
+                verbose: Some(false),
+                sources: HashMap::new(),
+            },
+            a
+        )
+    }
+}
